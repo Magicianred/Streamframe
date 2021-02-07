29caac040aa81c65bd817d25ab6c17701b2e5174
firdauskotp <firdauskotp@gmail.com>
2021-02-07 17:00:57 +0800

    initial

---
+++ flatform.js
@@ -0,0 +1,28 @@
+// 'use strict';
+// import React from "react";
+// import { FlatList } from "react-native";
+
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#ff');
+ReactDOM.render(e(LikeButton), domContainer);
